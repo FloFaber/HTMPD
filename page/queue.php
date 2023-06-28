c1c0ff945fd3b571a37493b3f3a350187e338194
Flo Faber <github.com@flofaber.com>
2023-06-28 15:54:52 +0200

    index.php is now a link to page/queue.php

--- page/queue.php
+++ page/queue.php
@@ -1,1 +1,26 @@
-../index.php
+<?php
+
+if(!file_exists(__DIR__ . "/../config.php")){
+  echo "ERROR: config.php not found. Please move config.example.php to config.php and configure correnctly.<br>\n";
+  die();
+}
+
+require_once __DIR__ . "/../config.php";
+
+$title = "Queue";
+$scripts = [ "js/queue.js" ];
+
+$body = "
+<div>
+  <button id='queue-clear'>clear</button>
+  <button id='queue-shuffle'>shuffle</button>
+  <button id='queue-save'>save...</button>
+  <button id='queue-add'>add...</button>
+</div>
+
+<div id='queue'>
+  <p style='color: grey;'>The Queue is empty. You can load <a href='" . WEBROOT . "/page/playlists.php'>Playlists</a> or songs from the <a href='" . WEBROOT . "/page/library.php'>Library</a>.
+</div>
+";
+
+require __DIR__ . "/../templates/main.html.php";
