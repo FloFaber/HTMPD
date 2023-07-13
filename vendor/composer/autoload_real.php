2965f9d5eb48920b2bf55452b63b3d5534f14ede
Flo Faber <github.com@flofaber.com>
2023-07-13 12:59:38 +0200

    Installed twig

--- vendor/composer/autoload_real.php
+++ vendor/composer/autoload_real.php
@@ -33,6 +33,18 @@ class ComposerAutoloaderInite50f82b55564f259fdcda5e25f28c684
 
         $loader->register(true);
 
+        $filesToLoad = \Composer\Autoload\ComposerStaticInite50f82b55564f259fdcda5e25f28c684::$files;
+        $requireFile = \Closure::bind(static function ($fileIdentifier, $file) {
+            if (empty($GLOBALS['__composer_autoload_files'][$fileIdentifier])) {
+                $GLOBALS['__composer_autoload_files'][$fileIdentifier] = true;
+
+                require $file;
+            }
+        }, null, null);
+        foreach ($filesToLoad as $fileIdentifier => $file) {
+            $requireFile($fileIdentifier, $file);
+        }
+
         return $loader;
     }
 }
